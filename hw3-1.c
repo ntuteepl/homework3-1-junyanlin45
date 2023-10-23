33f660ba4c1ae8a5ac739e03f9cfdedd8bcda609
junyanlin45 <146065138+junyanlin45@users.noreply.github.com>
2023-10-23 18:25:43 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+  int j,k=0;
+  scanf("%d",&j);
+
+  for(int i=2;i<j;j++)
+  {
+      if(j%i==0)
+      {
+
+          k=1;
+          break;
+
+
+      }
+
+       else
+      {
+          if(k==0)
+          {
+              printf("YES\n");
+          }
+          break;
+
+
+      }
+
+
+
+
+
+  }
+
+
+  if(k==1)
+        printf("NO\n");
+
+if(j==2)
+        printf("YES\n");
+
+
+
+
+
+}
