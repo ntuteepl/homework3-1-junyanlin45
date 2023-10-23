fc69cb1cbb16fbb08baa19476e34cbdae1eebd92
junyanlin45 <146065138+junyanlin45@users.noreply.github.com>
2023-10-23 18:26:40 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+int main() {
+    int s1, d1, s2, d2, s3, d3;
+    int a= 1;
+
+
+    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
+
+    // 首先，按返回时间从早到晚对订单进行排序
+    if (d1 > d2) {
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+        temp = d1;
+        d1 = d2;
+        d2 = temp;
+    }
+    if (d2 > d3) {
+        int temp = s2;
+        s2 = s3;
+        s3 = temp;
+        temp = d2;
+        d2 = d3;
+        d3 = temp;
+    }
+    if (d1 > d2) {
+        int temp = s1;
+        s1 = s2;
+        s2 = temp;
+        temp = d1;
+        d1 = d2;
+        d2 = temp;
+    }
+
+    if(s2<d1)
+   {
+       a=a+1;
+
+   }
+    if(s3<d1)
+   {
+       a=a+1;
+   }
+   printf("%d",a);
+}
 
